39d7643a2852ec5b8b5e5ceb2b2729fa8d9a99da
Chris Dostert <chrisdostert@users.noreply.github.com>
2017-07-22 13:18:08 -0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -4,12 +4,24 @@ set -e
 
 echo "starting docker daemon"
 nohup dockerd \
---host=unix:///var/run/docker.sock \
---host=tcp://0.0.0.0:2375 \
---storage-driver=overlay2 &
+  --host=unix:///var/run/docker.sock \
+  --storage-driver=aufs &
 
-echo ""
-sleep 2
+# poll for docker daemon up
+max_retries=2
+n=0
+until [ $n -ge $max_retries ]
+do
+  docker ps && break
+  n=$((n+1))
+  sleep 3
+done
+
+if [ "$n" -eq "$max_retries" ]; then
+  # assume failed
+  cat nohup.out
+  exit 1
+fi
 
 echo "loading image"
 docker image load -i /image.tar
